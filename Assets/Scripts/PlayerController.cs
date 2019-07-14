2840ffd6d53ac10d23683e8d9b9985d471c12b39
zyad osseyran <zyadosseyran@gmail.com>
2019-07-14 18:51:24 +0200

    first code

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+	public float speed;
+	public Text countText;
+	public Text winText;
+	private Rigidbody rb;
+	private int count = 0;
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+    		winText.text = "";
+		
+	}
+	void FixedUpdate(){
+		float moveHorizontal 	= Input.GetAxis("Horizontal");
+		float moveVeritcal 		= Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVeritcal);
+		rb.AddForce(movement * speed);
+
+
+	}
+	  void OnTriggerEnter(Collider other)  {
+         if (other.gameObject.CompareTag ("pickup")){
+            other.gameObject.SetActive (false);
+           	count = count + 1;
+           	setCounttext();
+        }
+    }
+    void setCounttext(){
+    	countText.text = "Counts: " + count.ToString();
+    	if(count == 4){
+    		countText.text = "";
+    		winText.text = "You won";
+
+    	}
+    }
+}
